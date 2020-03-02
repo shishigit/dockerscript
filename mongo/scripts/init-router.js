62ef326b9c6e5f55698c105eda595b2fd107372e
chendaliang <piaoyaocdl@163.com>
2020-03-02 13:15:12 +0800

    1

--- mongo/scripts/init-router.js
+++ mongo/scripts/init-router.js
@@ -1,8 +1,5 @@
-sh.addShard("shard01/shard01a:27017")
-sh.addShard("shard01/shard01b:27017")
+sh.addShard("shard01/shard01a:27017,shard01b:27017")
 
-sh.addShard("shard02/shard02a:27017")
-sh.addShard("shard02/shard02b:27017")
+sh.addShard("shard02/shard02a:27017,shard02b:27017")
 
-sh.addShard("shard03/shard03a:27017")
-sh.addShard("shard03/shard03b:27017")
+sh.addShard("shard03/shard03a:27017,shard03b:27017")
