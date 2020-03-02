476899bae77ae9a2fa4f2d6e48e56cc42e0b43f5
chendaliang <piaoyaocdl@163.com>
2020-03-02 13:40:59 +0800

    1

--- mongo/init.sh
+++ mongo/init.sh
@@ -5,4 +5,4 @@ docker-compose exec shard01a sh -c "mongo --port 27017 < /scripts/init-shard01.j
 docker-compose exec shard02a sh -c "mongo --port 27017 < /scripts/init-shard02.js"
 docker-compose exec shard03a sh -c "mongo --port 27017 < /scripts/init-shard03.js"
 sleep 20
-docker-compose exec router sh -c "mongo < /scripts/init-router.js"
+docker-compose exec router sh -c "mongo --port 27017 < /scripts/init-router.js"
