8b37bd55e59db1199454c394f1e09ab61c3c8064
chendaliang <piaoyaocdl@163.com>
2020-03-02 16:42:11 +0800

    1

--- mongo/scripts/init-router.js
+++ mongo/scripts/init-router.js
@@ -3,3 +3,13 @@ sh.addShard("shard01/shard01a:27017,shard01b:27017")
 sh.addShard("shard02/shard02a:27017,shard02b:27017")
 
 sh.addShard("shard03/shard03a:27017,shard03b:27017")
+
+use admin
+
+db.createUser(
+    {
+        user: "root",
+        pwd: 'shishi',
+        roles: [ "userAdminAnyDatabase", "readWriteAnyDatabase"]
+    }
+)
